3a7f1137a59fc606ada6171c63aa4fefdda85b52
Chongsau Reang <117213236+Lauremxd@users.noreply.github.com>
2025-04-14 16:55:43 +0530

    added as index.html

---
+++ form.js
@@ -0,0 +1,45 @@
+function validate() {
+    let name = document.getElementById("name").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    let nameError = document.getElementById("nameError");
+    let passwordError = document.getElementById("passwordError");
+
+    let isValid = true;
+
+    nameError.textContent = "";
+    passwordError.textContent = "";
+
+   const correctName = "Laurem";
+    const correctPassword = "986264";
+
+    {
+    }
+    if (name === "") {
+        nameError.textContent = "Naam likhna zaroori hai.";
+        isValid = false;
+    } else if (name !== correctName) {
+        nameError.textContent = "username galat hai bhai.";
+        isValid = false;
+    }
+
+    if (password === "") {
+        passwordError.textContent = "Password likhna zaroori hai.";
+        isValid = false;
+    } else if (password !== correctPassword) {
+        passwordError.textContent = "Password bhi galat hai.";
+        isValid = false;
+    }
+
+    if (isValid) {
+        alert("Login successful!");
+        document.getElementById("loginForm").reset();
+    }
+
+    return false;
+}
+
+function togglePassword() {
+    let passwordInput = document.getElementById("password");
+    passwordInput.type = passwordInput.type === "password" ? "text" : "password";
+}
